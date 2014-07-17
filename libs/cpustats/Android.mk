7938a6820ea945191ce40aa5167036d914a850cc
steppnasty <steppnasty@gmail.com>
2014-07-17 10:25:35 -0400

    libcpustats: Updated to jb-mr1.1-release

--- libs/cpustats/Android.mk
+++ libs/cpustats/Android.mk
@@ -9,13 +9,3 @@ LOCAL_SRC_FILES :=     \
 LOCAL_MODULE := libcpustats
 
 include $(BUILD_STATIC_LIBRARY)
-
-#include $(CLEAR_VARS)
-#
-#LOCAL_SRC_FILES :=     \
-#       CentralTendencyStatistics.cpp \
-#       ThreadCpuUsage.cpp
-#
-#LOCAL_MODULE := libcpustats
-#
-#include $(BUILD_HOST_STATIC_LIBRARY)
